d2a7331eda94790dfc5bf0dd5ae4ba754728cb4d
fredOpenscop <frederic.bouton@openscop.fr>
2020-10-06 09:56:22 +0200

    [#17] correction requete multi site > correction des clause where
    [f] fix methode where des requetes

--- plugin/openscop/helpers/CourseHelper.php
+++ plugin/openscop/helpers/CourseHelper.php
@@ -16,25 +16,36 @@ class CourseHelper extends CourseManager
 {
     /**
      * retourne la liste des cours pour la page d'acceuil
+     *
      * @return array
      */
-    public static function getHomeCourses(){
-        $currentSiteId = api_get_current_access_url_id();
-        $userId = api_get_user_id();
-        $courses = self::getCoursesForSite($currentSiteId);
-        $courses = self::processLastCourse($courses);
+    public static function getHomeCourses()
+    {
+        try {
+            $currentSiteId = api_get_current_access_url_id();
+            $courses = self::getCoursesForSite($currentSiteId);
+            $courses = self::processLastCourse($courses);
+        } catch(Exception $e) {
+            return [];
+        }
         return $courses;
     }
 
-    public static function getUserCourses(){
-        $currentSiteId = api_get_current_access_url_id();
-        $userId = api_get_user_id();
-        $courses = self::getCoursesForSiteAndUser($currentSiteId, $userId);
-        $courses = self::processLastCourse($courses);
+    public static function getUserCourses()
+    {
+        try {
+            $currentSiteId = api_get_current_access_url_id();
+            $userId = api_get_user_id();
+            $courses = self::getCoursesForSiteAndUser($currentSiteId, $userId);
+            $courses = self::processLastCourse($courses);
+        } catch(Exception $e) {
+            return [];
+        }
         return $courses;
     }
 
-    public static function getCoursesForSite($siteId){
+    public static function getCoursesForSite($siteId)
+    {
         $table_course = Database::get_main_table(TABLE_MAIN_COURSE);
         $table_course_url = Database::get_main_table(TABLE_MAIN_ACCESS_URL_REL_COURSE);
         $now = api_get_utc_datetime();
@@ -43,14 +54,17 @@ class CourseHelper extends CourseManager
             ->select('*')
             ->from($table_course, 'c')
             ->leftJoin($table_course_url, 'aurc', Doctrine\ORM\Query\Expr\Join::ON, 'c.id = aurc.c_id')
-            ->where('aurc.access_url_id = '.$siteId)
-            ->where('creation_date <= '. $now)
-            ->where('visibility <> '. COURSE_VISIBILITY_CLOSED)
-            ->where('visibility <> '. COURSE_VISIBILITY_HIDDEN)
+            ->where('aurc.access_url_id = ' . $siteId)
+            ->andWhere('creation_date <= "' . $now . '"')
+            ->andWhere('visibility <> ' . COURSE_VISIBILITY_CLOSED)
+            ->andWhere('visibility <> ' . COURSE_VISIBILITY_HIDDEN)
+            ->groupBy('c.id')
             ->orderBy('c.id', 'DESC');
         return Database::query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-    public static function getCoursesForSiteAndUser($siteId, $userId){
+
+    public static function getCoursesForSiteAndUser($siteId, $userId)
+    {
         $table_course = Database::get_main_table(TABLE_MAIN_COURSE);
         $table_course_url = Database::get_main_table(TABLE_MAIN_ACCESS_URL_REL_COURSE);
         $tableCourseUser = Database::get_main_table(TABLE_MAIN_COURSE_USER);
@@ -61,30 +75,31 @@ class CourseHelper extends CourseManager
             ->from($table_course, 'c')
             ->leftJoin($table_course_url, 'aurc', Doctrine\ORM\Query\Expr\Join::ON, 'c.id = aurc.c_id')
             ->leftJoin($tableCourseUser, 'cru', Doctrine\ORM\Query\Expr\Join::ON, 'c.id = cru.c_id')
-            ->where('aurc.access_url_id = '.$siteId)
-            ->where('creation_date <= '. $now)
-            ->where('visibility <> '. COURSE_VISIBILITY_CLOSED)
-            ->where('visibility <> '. COURSE_VISIBILITY_HIDDEN)
-            ->where('cru.user_id = '. $userId)
+            ->where('aurc.access_url_id = ' . $siteId)
+            ->andWhere('creation_date <= ' . $now)
+            ->andWhere('visibility <> ' . COURSE_VISIBILITY_CLOSED)
+            ->andWhere('visibility <> ' . COURSE_VISIBILITY_HIDDEN)
+            ->andWhere('cru.user_id = ' . $userId)
             ->orderBy('c.id', 'DESC');
         return Database::query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-    public static function getNewCourse(){
+
+    public static function getNewCourse()
+    {
         $limit = 6;
         $userId = api_get_user_id();
         // Getting my courses
         $my_course_list = self::get_courses_list_by_user_id($userId);
 
 
-
         $codeList = [];
-        foreach ($my_course_list as $course) {
+        foreach($my_course_list as $course) {
             $codeList[$course['real_id']] = $course['real_id'];
         }
 
-        if (api_is_drh()) {
+        if(api_is_drh()) {
             $courses = self::get_courses_followed_by_drh($userId);
-            foreach ($courses as $course) {
+            foreach($courses as $course) {
                 $codeList[$course['real_id']] = $course['real_id'];
             }
         }
@@ -102,8 +117,8 @@ class CourseHelper extends CourseManager
                 WHERE
                     creation_date <= '$now' AND
                     creation_date > DATE_SUB('$now', INTERVAL 30 DAY) AND
-                    visibility <> ".COURSE_VISIBILITY_CLOSED." AND
-                    visibility <> ".COURSE_VISIBILITY_HIDDEN."
+                    visibility <> " . COURSE_VISIBILITY_CLOSED . " AND
+                    visibility <> " . COURSE_VISIBILITY_HIDDEN . "
                 GROUP BY u.c_id
                 ORDER BY creation_date DESC
                 LIMIT $limit
@@ -111,20 +126,21 @@ class CourseHelper extends CourseManager
 
         $result = Database::query($sql);
         $courses = [];
-        if (Database::num_rows($result)) {
+        if(Database::num_rows($result)) {
             $courses = Database::store_result($result, 'ASSOC');
             $courses = self::processLastCourse($courses, $codeList);
         }
         return $courses;
     }
 
-    public static function processLastCourse($courses, $codeList = []){
+    public static function processLastCourse($courses, $codeList = [])
+    {
         $hotCourses = [];
-        $ajax_url = api_get_path(WEB_AJAX_PATH).'course.ajax.php?a=add_course_vote';
+        $ajax_url = api_get_path(WEB_AJAX_PATH) . 'course.ajax.php?a=add_course_vote';
         $stok = Security::get_existing_token();
         $user_id = api_get_user_id();
 
-        foreach ($courses as $courseId) {
+        foreach($courses as $courseId) {
             $course_info = api_get_course_info_by_id($courseId['c_id']);
             $courseCode = $course_info['code'];
             $categoryCode = !empty($course_info['categoryCode']) ? $course_info['categoryCode'] : "";
@@ -139,8 +155,8 @@ class CourseHelper extends CourseManager
             );
 
             $sessionId = api_get_session_id();
-            $cidReq = 'cidReq='.$my_course['id'];
-            $session = '&id_session='.$sessionId;
+            $cidReq = 'cidReq=' . $my_course['id'];
+            $session = '&id_session=' . $sessionId;
             $action = '&action=view';
 
             $userRegisteredInCourse = self::is_user_subscribed_in_course($user_id, $course_info['code']);
@@ -151,8 +167,8 @@ class CourseHelper extends CourseManager
 
             // main/lp/lp_controller.php?cidReq=TEST&id_session=0&gidReq=0&gradebook=0&origin=&action=view&lp_id=1&isStudentView=true
             $id = $lp['id'];
-            $url_start_lp = 'main/lp/lp_controller.php?'.$cidReq.$session.$action.'&lp_id='.$id;
-            $url_start_lp .= '&isStudentView='.!$userRegisteredInCourseAsTeacher;
+            $url_start_lp = 'main/lp/lp_controller.php?' . $cidReq . $session . $action . '&lp_id=' . $id;
+            $url_start_lp .= '&isStudentView=' . !$userRegisteredInCourseAsTeacher;
             $my_course['course_student_url'] = $url_start_lp;
             $my_course['course_teacher_url'] = api_get_course_url($my_course['id']);
 
@@ -166,10 +182,10 @@ class CourseHelper extends CourseManager
             $textPercent = $percent . '%';
             $my_course['progress'] = learnpath::get_progress_bar($textPercent);
             // Course visibility
-            if ($access_link && in_array('register', $access_link)) {
-                $my_course['course_register_url'] = api_get_path(WEB_COURSE_PATH).$course_info['path'].'/index.php?action=subscribe&sec_token='.$stok;
+            if($access_link && in_array('register', $access_link)) {
+                $my_course['course_register_url'] = api_get_path(WEB_COURSE_PATH) . $course_info['path'] . '/index.php?action=subscribe&sec_token=' . $stok;
                 $my_course['register_button'] = Display::url(
-                    get_lang('Subscribe').' '.
+                    get_lang('Subscribe') . ' ' .
                     Display::returnFontAwesomeIcon('sign-in'),
                     $my_course['course_register_url'],
                     [
@@ -180,16 +196,16 @@ class CourseHelper extends CourseManager
                 );
             }
 
-            if (($access_link && in_array('enter', $access_link) ||
-                $course_info['visibility'] == COURSE_VISIBILITY_OPEN_WORLD) && ($my_course['is_course_student'] || $my_course['is_course_teacher'])
+            if(($access_link && in_array('enter', $access_link) ||
+                    $course_info['visibility'] == COURSE_VISIBILITY_OPEN_WORLD) && ($my_course['is_course_student'] || $my_course['is_course_teacher'])
             ) {
-                if($my_course['is_course_teacher']){
+                if($my_course['is_course_teacher']) {
                     $url = $my_course['course_teacher_url'];
-                }else{
+                } else {
                     $url = $url_start_lp;
                 }
                 $my_course['go_to_course_button'] = Display::url(
-                    get_lang('GoToCourse').' '.
+                    get_lang('GoToCourse') . ' ' .
                     Display::returnFontAwesomeIcon('share'),
                     $url,
                     [
@@ -200,11 +216,11 @@ class CourseHelper extends CourseManager
                 );
             }
 
-            if ($access_link && in_array('unsubscribe', $access_link) && $my_course['is_course_student'] && $my_course['is_course_teacher'] == false) {
-                $my_course['course_unsubscribe_url'] = api_get_path(WEB_CODE_PATH).'auth/courses.php?action=unsubscribe&course_code='.$courseCode
-                    .'&sec_token='.$stok.'&category_code='.$categoryCode;
+            if($access_link && in_array('unsubscribe', $access_link) && $my_course['is_course_student'] && $my_course['is_course_teacher'] == false) {
+                $my_course['course_unsubscribe_url'] = api_get_path(WEB_CODE_PATH) . 'auth/courses.php?action=unsubscribe&course_code=' . $courseCode
+                    . '&sec_token=' . $stok . '&category_code=' . $categoryCode;
                 $my_course['unsubscribe_button'] = Display::url(
-                    get_lang('Unreg').' '.
+                    get_lang('Unreg') . ' ' .
                     Display::returnFontAwesomeIcon('sign-out'),
                     $my_course['course_unsubscribe_url'],
                     [
@@ -222,11 +238,11 @@ class CourseHelper extends CourseManager
                 $course_info['real_id'],
                 BuyCoursesPlugin::PRODUCT_TYPE_COURSE
             );
-            if ($isThisCourseInSale) {
+            if($isThisCourseInSale) {
                 // set the price label
                 $my_course['price'] = $isThisCourseInSale['html'];
                 // set the Buy button instead register.
-                if ($isThisCourseInSale['verificator'] && !empty($my_course['register_button'])) {
+                if($isThisCourseInSale['verificator'] && !empty($my_course['register_button'])) {
                     $my_course['register_button'] = $plugin->returnBuyCourseButton(
                         $course_info['real_id'],
                         BuyCoursesPlugin::PRODUCT_TYPE_COURSE
@@ -240,18 +256,18 @@ class CourseHelper extends CourseManager
             $my_course['teachers'] = self::getTeachersFromCourse($course_info['real_id'], true);
             $point_info = self::get_course_ranking($course_info['real_id'], 0);
             $my_course['rating_html'] = '';
-            if (api_get_configuration_value('hide_course_rating') === false) {
+            if(api_get_configuration_value('hide_course_rating') === false) {
                 $my_course['rating_html'] = Display::return_rating_system(
-                    'star_'.$course_info['real_id'],
-                    $ajax_url.'&course_id='.$course_info['real_id'],
+                    'star_' . $course_info['real_id'],
+                    $ajax_url . '&course_id=' . $course_info['real_id'],
                     $point_info
                 );
             }
             if($my_course['is_course_teacher']) {
                 $my_course['public_url'] = $my_course['course_teacher_url'];
-            }else if($my_course['is_course_student']) {
-                    $my_course['public_url'] = $my_course['course_student_url'];
-            }else if($my_course['register_button']){
+            } else if($my_course['is_course_student']) {
+                $my_course['public_url'] = $my_course['course_student_url'];
+            } else if($my_course['register_button']) {
                 $my_course['public_url'] = $my_course['course_register_url'];
             }
 
@@ -261,12 +277,13 @@ class CourseHelper extends CourseManager
         return $hotCourses;
     }
 
-    public static function getParcoursForCourse($courseId){
+    public static function getParcoursForCourse($courseId)
+    {
         $learningPaths = Database::getManager()
             ->createQueryBuilder()
             ->select('*')
             ->from('c_lp', 'lp')
-            ->where('c_id = '.$courseId)
+            ->where('c_id = ' . $courseId)
             ->orderBy('id', 'ASC');
         return Database::fetch_assoc(Database::query($learningPaths));
     }
