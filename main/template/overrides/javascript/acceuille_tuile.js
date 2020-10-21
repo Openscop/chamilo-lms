8e23296df2feb4485963644bd8327c6c9878ae67
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-10-21 17:17:28 +0200

    refactor: tuile colors

--- main/template/overrides/javascript/acceuille_tuile.js
+++ main/template/overrides/javascript/acceuille_tuile.js
@@ -1,5 +1,5 @@
 $(function() {
-    color_tuile();
+    // color_tuile();
     var is_display = 0;
     $(".tuile").click(function(e){
         // stop listener si click sur bouton de rating :
@@ -41,19 +41,19 @@ $(function() {
 
 let nmb = 0;
 
-function color_tuile(){
-    let color = ["background_1", "background_2", "background_3", "background_4", "background_5", "background_6"];
-    // for( var i =0; i < $(".tuile").length; i += 1){
-    //     let ancient_nmb = 6;
-    //     nmb = Math.floor(Math.random() * 4);
-    //     if(nmb === ancient_nmb){
-    //         nmb = Math.floor(Math.random() * 4);
-    //     }
-    //     $(".tuile").eq( i ).find(".block-title").addClass(color[nmb]);
-    //     ancient_nmb = nmb;
-    // }
-    for( var i =0; i < 6; i += 1) {
-        // $(".container-fluid .tuile:nth-child(i)").find(".block-title").addClass(color[i]);
-        $(".tuile").eq( i ).find(".block-title").addClass(color[i]);
-    }
-}
+// function color_tuile(){
+//     let color = ["background_1", "background_2", "background_3", "background_4", "background_5", "background_6"];
+//     // for( var i =0; i < $(".tuile").length; i += 1){
+//     //     let ancient_nmb = 6;
+//     //     nmb = Math.floor(Math.random() * 4);
+//     //     if(nmb === ancient_nmb){
+//     //         nmb = Math.floor(Math.random() * 4);
+//     //     }
+//     //     $(".tuile").eq( i ).find(".block-title").addClass(color[nmb]);
+//     //     ancient_nmb = nmb;
+//     // }
+//     for( var i =0; i < 6; i += 1) {
+//         // $(".container-fluid .tuile:nth-child(i)").find(".block-title").addClass(color[i]);
+//         $(".tuile").eq( i ).find(".block-title").addClass(color[i]);
+//     }
+// }
